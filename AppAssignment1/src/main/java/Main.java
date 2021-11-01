a3b648ecd154e250a46d7f0a1c32a153d9400e34
RMaharaj21 <55695895+RMaharaj21@users.noreply.github.com>
2021-11-01 19:29:06 -0400

    Add files via upload

---
+++ AppAssignment1/src/main/java/Main.java
@@ -0,0 +1,50 @@
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 1
+ *  Copyright 2021 Ryan Maharaj
+ */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        intro();
+
+        int choice;
+
+    }
+
+    public static void intro() {
+        System.out.println(" To Do List Application");
+        System.out.println("        Welcome        ");
+        System.out.println("----------------------------");
+        System.out.println(" Please select an option");
+        System.out.println("  1. Add a To Do list");
+        System.out.println("  2. Edit a To Do list");
+        System.out.println("  3. Delete a To Do list");
+        System.out.println("----------------------------");
+
+        //Determine what user wants to do
+
+    }
+
+    public static void arraylist(String[] args) {
+        //creating an array list for storing the tasks
+
+    }
+
+    public static void arraylist2(String[] args) {
+        //creating an array list for storing each of the 100 tasks in each input
+
+    }
+
+    public static void input(String[] args) {
+        //Get input for a title//
+        //Get input for a description less than 256 characrs
+        //Get input for the due dat in format YYYY-MM-DD
+
+    }
+    public static void delete(String[] args) {
+        //method for deleting tasks
+
+    }
+}
